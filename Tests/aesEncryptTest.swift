44fc18ec9dd385e7d82c2f0d332528cf06a8ff7e
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-02 19:16:19 -0700

    extract secureRandom

--- Tests/aesEncryptTest.swift
+++ Tests/aesEncryptTest.swift
@@ -10,6 +10,14 @@ import CommonCrypto
 @testable import EncryptCard
 
 class aesEncryptTest: XCTestCase {
+    func secureRandom(size: Int) throws -> Data {
+        var bytes = [UInt8](repeating: 0, count: size)
+        let status = SecRandomCopyBytes(kSecRandomDefault, size, &bytes)
+        guard status == errSecSuccess else {
+            throw NSError(domain: NSOSStatusErrorDomain, code: Int(status))
+        }
+        return Data(bytes)
+    }
     func testSecureRandom() {
         let count = 10
         var bytes = [UInt8](repeating: 0, count: count)
