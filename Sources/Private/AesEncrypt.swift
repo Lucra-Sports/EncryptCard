128c220a364e3f6e775aa88c7583480a203030b8
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-02 23:05:36 -0700

    move source to private

---
+++ Sources/Private/AesEncrypt.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  
+//
+//  Created by Paul Zabelin on 5/2/22.
+//
+
+import Foundation
+import CommonCrypto
+
+func aesEncrypt(key: Data, seed: Data, string: String) throws -> String {
+    let padLength = kCCBlockSizeAES128
+    var result = Data(repeating: .zero, count: string.count + padLength)
+    var resultLength: size_t = 0
+    let error = result.withUnsafeMutableBytes { buffer  in
+        seed.withUnsafeBytes { seedBytes in
+            key.withUnsafeBytes { keyBytes in
+                string.utf8CString.withUnsafeBytes { stringBuffer in
+                    CCCrypt(
+                        CCOperation(kCCEncrypt),
+                        CCAlgorithm(kCCAlgorithmAES),
+                        CCOptions(kCCOptionPKCS7Padding),
+                        keyBytes.baseAddress, key.count,
+                        seedBytes.baseAddress,
+                        stringBuffer.baseAddress, string.count,
+                        buffer.baseAddress!, buffer.count,
+                        &resultLength
+                    )
+                }
+            }
+        }
+    }
+    guard error == kCCSuccess else {
+        throw NSError(domain: NSOSStatusErrorDomain, code: Int(error))
+    }
+    return result[0..<resultLength].base64EncodedString()
+}
+
--- Tests/aesEncryptTest.swift
+++ Tests/aesEncryptTest.swift
@@ -15,7 +15,7 @@ class aesEncryptTest: XCTestCase {
         let data = secureRandom(size: count)
         XCTAssertTrue(data.contains(UInt8.max), "should include max")
         XCTAssertTrue(data.contains(UInt8.min), "should include main")
-        let average = UInt8(data.bytes.map(Int.init).reduce(0, +) / count)
+        let average = UInt8(data.map(Int.init).reduce(0, +) / count)
         XCTAssertEqual(average, UInt8.max / 2, accuracy: UInt8.max / 4,
                        "average should be middle value with 0.25 of range accuracy")
     }
