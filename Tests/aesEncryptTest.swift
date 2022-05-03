b3775af9312934e51dccbfeb8ff892e5e61d4196
Paul Zabelin <paulz@users.noreply.github.com>
2022-05-02 19:07:49 -0700

    extract AES encrypt

--- Tests/aesEncryptTest.swift
+++ Tests/aesEncryptTest.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import CommonCrypto
+@testable import EncryptCard
 
 class aesEncryptTest: XCTestCase {
     func testSecureRandom() {
@@ -17,27 +18,6 @@ class aesEncryptTest: XCTestCase {
         let average = UInt8(bytes.map(Int.init).reduce(0, +) / count)
         XCTAssertEqual(average, UInt8.max / 2, accuracy: UInt8.max / 4)
     }
-    func aesEncrypt(key: Data, seed: Data, string: String) throws -> String {
-        let padLength = kCCBlockSizeAES128
-        var result = Data(repeating: .zero, count: string.count + padLength)
-        var resultLength: size_t = 0
-        let error = result.withUnsafeMutableBytes { buffer  in
-            CCCrypt(
-                CCOperation(kCCEncrypt),
-                CCAlgorithm(kCCAlgorithmAES),
-                CCOptions(kCCOptionPKCS7Padding),
-                key.bytes, key.count,
-                seed.bytes,
-                string.bytes, string.count,
-                buffer.baseAddress!, buffer.count,
-                &resultLength
-            )
-        }
-        guard error == kCCSuccess else {
-            throw NSError(domain: NSOSStatusErrorDomain, code: Int(error))
-        }
-        return result[0..<resultLength].base64EncodedString()
-    }
     func testAesEncryptReturnsStablePaddedResult() throws {
         let keyData = Data(repeating: 0, count: kCCKeySizeAES256)
         let ivData = Data(repeating: 0, count: kCCBlockSizeAES128)
@@ -46,28 +26,5 @@ class aesEncryptTest: XCTestCase {
         XCTAssertEqual(encrypted, "fg51d++cMKa/CyXgYh6CfklCobeQJP2bzEQPAlt4hLk=")
         let encData = try XCTUnwrap(Data(base64Encoded: encrypted))
         XCTAssertEqual(encData.count % kCCBlockSizeAES128, 0, "should be in blocks")
-    }
-    func testAesEncrypt() {
-        let keyData = String(repeating: "K", count: kCCKeySizeAES256)
-        let ivData = String(repeating: "I", count: kCCBlockSizeAES128)
-        let inputData = String(repeating: "D", count: 20)
-        let padLength = kCCBlockSizeAES128
-        var result = Data(repeating: .zero, count: inputData.count + padLength)
-        var resultLength: size_t = 0
-        let error = result.withUnsafeMutableBytes { buffer  in
-            CCCrypt(
-                CCOperation(kCCEncrypt),
-                CCAlgorithm(kCCAlgorithmAES),
-                CCOptions(kCCOptionPKCS7Padding),
-                keyData.bytes, keyData.count,
-                ivData.bytes,
-                inputData.bytes, inputData.count,
-                buffer.baseAddress!, buffer.count,
-                &resultLength
-            )
-        }
-        XCTAssertEqual(error, CCCryptorStatus(kCCSuccess))
-        XCTAssertLessThanOrEqual(resultLength, result.count)
-        XCTAssertEqual(result.base64EncodedString(), "MN6IsJr+G6WkLXPNEHGBa/mgy6x+ZTHemZ71JBMkooUAAAAA")
     }
 }
